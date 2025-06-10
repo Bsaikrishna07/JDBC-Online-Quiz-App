0ff8e87dc7124a57d235c23e68a604cf4205375a
Bsaikrishna07 <saikrishnab0729@gmail.com>
2025-06-10 14:26:53 +0530

    Update DBConnection.java

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -5,6 +5,6 @@ public class DBConnection {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/quizdb", "root", "root");
+            "jdbc:mysql://localhost:3306/quizdb", "root", "password");
     }
 }
